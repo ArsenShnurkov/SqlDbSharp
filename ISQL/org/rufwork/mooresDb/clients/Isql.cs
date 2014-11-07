9d0f5a61709e3d8011c61016ae1a122814ca4fc3
Arsen Shnurkov <Arsen.Shnurkov@gmail.com>
2014-11-08 00:58:48 +0300

    So

--- ISQL/org/rufwork/mooresDb/clients/Isql.cs
+++ ISQL/org/rufwork/mooresDb/clients/Isql.cs
@@ -79,7 +79,10 @@ SqlDbSharp.config, placed in this folder:
                     while (!strInput.Equals("."))
                     {
                         strInput = Console.ReadLine();
-
+						if (strInput == null) // end of file
+						{
+							break;
+						}
                         if (strInput.Contains("--"))
                         {
                             strInput = strInput.Substring(0, strInput.IndexOf("--"));
@@ -87,142 +90,144 @@ SqlDbSharp.config, placed in this folder:
                         strCmd += strInput + " ";
                     }
 
-                    if (!strCmd.Trim().Equals("."))
+					if (string.IsNullOrEmpty(strCmd) || strCmd.Trim().Equals("."))
                     {
-                        Queue<string> qCmds = strCmd.Trim(' ').Trim('.').SplitSeeingSingleQuotesAndBackticks(";", true);
+						break;
+					}
 
-                        foreach (string strSingleCommand in qCmds)
+                    Queue<string> qCmds = strCmd.Trim(' ').Trim('.').SplitSeeingSingleQuotesAndBackticks(";", true);
+                    foreach (string strSingleCommand in qCmds)
+                    {
+                        strCmd = strSingleCommand.Substring(0, strSingleCommand.LastIndexOf(";") + 1).Trim();    // kinda kludging to reuse code for now.
+
+                        // I'm going to cheat and set up some test strings,
+                        // just to save some typing.
+                        // Ob remove when live.
+                        switch (strCmd)
                         {
-                            strCmd = strSingleCommand.Substring(0, strSingleCommand.LastIndexOf(";") + 1).Trim();    // kinda kludging to reuse code for now.
-
-                            // I'm going to cheat and set up some test strings,
-                            // just to save some typing.
-                            // Ob remove when live.
-                            switch (strCmd)
-                            {
-                                case "test usage;":
-                                    DatabaseContext database = new DatabaseContext(@"C:\temp\DatabaseName"); // or Mac path, etc.
-                                    CommandParser parser = new CommandParser(database);
-                                    string sql = @"CREATE TABLE TestTable (
-                                            ID INTEGER (4) AUTOINCREMENT,
-                                            CITY CHAR (10));";
-                                    parser.executeCommand(sql);
-                                    parser.executeCommand("INSERT INTO TestTable (CITY) VALUES ('New York');");
-                                    parser.executeCommand("INSERT INTO TestTable (CITY) VALUES ('Boston');");
-                                    parser.executeCommand("INSERT INTO TestTable (CITY) VALUES ('Fuquay');");
-                                    sql = "SELECT * FROM TestTable;";
-                                    DataTable dataTable = (DataTable)parser.executeCommand(sql);
-                                    Console.WriteLine(InfrastructureUtils.dataTableToString(dataTable));
-                                    parser.executeCommand("DROP TABLE TestTable;");
-
-                                    strCmd = "";
-                                    goto case "goto kludge";
-
-                                case "test create;":
-                                    strTestTableName = "jive" + DateTime.Now.Ticks;
-                                    strCmd = @"create TABLE " + strTestTableName + @"
-                                        (ID INTEGER (4) AUTOINCREMENT,
-                                        CITY CHAR (10),
-                                        STATE CHAR (2),
-                                        LAT_N REAL (5),
-                                        LONG_W REAL (5),
-                                        TIMESTAMP DATETIME(8));";
-                                    goto case "goto kludge";
-
-                                case "test create2;":
-                                    strTestTableName = "rest" + DateTime.Now.Ticks;
-                                    strCmd = @"create TABLE " + strTestTableName + @"
-                                        (
+                            case "test usage;":
+                                DatabaseContext database = new DatabaseContext(@"C:\temp\DatabaseName"); // or Mac path, etc.
+                                CommandParser parser = new CommandParser(database);
+                                string sql = @"CREATE TABLE TestTable (
                                         ID INTEGER (4) AUTOINCREMENT,
-                                        CityId INTEGER (4),
-                                        Name CHAR (30)
-                                        );";
-                                    goto case "goto kludge";
+                                        CITY CHAR (10));";
+                                parser.executeCommand(sql);
+                                parser.executeCommand("INSERT INTO TestTable (CITY) VALUES ('New York');");
+                                parser.executeCommand("INSERT INTO TestTable (CITY) VALUES ('Boston');");
+                                parser.executeCommand("INSERT INTO TestTable (CITY) VALUES ('Fuquay');");
+                                sql = "SELECT * FROM TestTable;";
+                                DataTable dataTable = (DataTable)parser.executeCommand(sql);
+                                Console.WriteLine(InfrastructureUtils.dataTableToString(dataTable));
+                                parser.executeCommand("DROP TABLE TestTable;");
 
-                                case "test select;":
-                                    strCmd = @"SELECT ID, CITY, LAT_N FROM "
-                                        + strTestTableName + @"
-                                        INNER JOIN rest
-                                        ON jive.Id = rest.CityId
-                                        WHERE city = 'Chucktown' AND LAT_N > 32;";
-                                    goto case "goto kludge";
+                                strCmd = "";
+                                goto case "goto kludge";
 
-                                case "test insert;":
-                                    strCmd = @"INSERT INTO " + strTestTableName + @" (CITY, STATE, LAT_N, LONG_W, TIMESTAMP)
-                                        VALUES ('Chucktown', 'SC', 32.776, 79.931, '12/12/2001');";
-                                    goto case "goto kludge";
+                            case "test create;":
+                                strTestTableName = "jive" + DateTime.Now.Ticks;
+                                strCmd = @"create TABLE " + strTestTableName + @"
+                                    (ID INTEGER (4) AUTOINCREMENT,
+                                    CITY CHAR (10),
+                                    STATE CHAR (2),
+                                    LAT_N REAL (5),
+                                    LONG_W REAL (5),
+                                    TIMESTAMP DATETIME(8));";
+                                goto case "goto kludge";
 
-                                case "insert2;":
-                                    strCmd = @"INSERT INTO " + strTestTableName + @" (CITY, LAT_N, LONG_W, TIMESTAMP) VALUES ('Chucktown', 32.776, 79.931, '11/11/2011');";
-                                    goto case "goto kludge";
+                            case "test create2;":
+                                strTestTableName = "rest" + DateTime.Now.Ticks;
+                                strCmd = @"create TABLE " + strTestTableName + @"
+                                    (
+                                    ID INTEGER (4) AUTOINCREMENT,
+                                    CityId INTEGER (4),
+                                    Name CHAR (30)
+                                    );";
+                                goto case "goto kludge";
 
-                                case "test drop;":
-                                    strCmd = @"DROP TABLE " + strTestTableName + @";";
-                                    goto case "goto kludge";
+                            case "test select;":
+                                strCmd = @"SELECT ID, CITY, LAT_N FROM "
+                                    + strTestTableName + @"
+                                    INNER JOIN rest
+                                    ON jive.Id = rest.CityId
+                                    WHERE city = 'Chucktown' AND LAT_N > 32;";
+                                goto case "goto kludge";
 
-                                case "test update;":
-                                    strCmd = @"UPDATE jive SET city = 'Gotham', LAT_N = 45.987 WHERE ID = 4;";
-                                    goto case "goto kludge";
+                            case "test insert;":
+                                strCmd = @"INSERT INTO " + strTestTableName + @" (CITY, STATE, LAT_N, LONG_W, TIMESTAMP)
+                                    VALUES ('Chucktown', 'SC', 32.776, 79.931, '12/12/2001');";
+                                goto case "goto kludge";
 
-                                // Okay, Lippert doesn't say to use it quite like this, but he did give me the idea:
-                                // http://blogs.msdn.com/b/ericlippert/archive/2009/08/13/four-switch-oddities.aspx
-                                case "goto kludge":
-                                    Console.WriteLine("Test cmd:" + Environment.NewLine + strCmd);
-                                    break;
-                            }
+                            case "insert2;":
+                                strCmd = @"INSERT INTO " + strTestTableName + @" (CITY, LAT_N, LONG_W, TIMESTAMP) VALUES ('Chucktown', 32.776, 79.931, '11/11/2011');";
+                                goto case "goto kludge";
 
-                            // Execute the command.
-                            if (strCmd.ToLower().StartsWith("use"))
-                            {
-                                string strDbPath = strCmd.Split()[1].TrimEnd(';');
-                                // Complete kludge.  Use regexp and check for xplat formats.
-                                if (strDbPath.StartsWith(@"C:\"))
-                                    strParentDir = strDbPath;
-                                else
-                                    strParentDir = Utils.cstrHomeDir + System.IO.Path.DirectorySeparatorChar + strDbPath;
-                                dbTemp = new DatabaseContext(strParentDir);
-                                Console.WriteLine(strParentDir);
-                            }
-                            else if (strParentDir.Equals(""))
-                            {
-                                Console.WriteLine("Please select a database before executing a statement.");
-                            }
+                            case "test drop;":
+                                strCmd = @"DROP TABLE " + strTestTableName + @";";
+                                goto case "goto kludge";
+
+                            case "test update;":
+                                strCmd = @"UPDATE jive SET city = 'Gotham', LAT_N = 45.987 WHERE ID = 4;";
+                                goto case "goto kludge";
+
+                            // Okay, Lippert doesn't say to use it quite like this, but he did give me the idea:
+                            // http://blogs.msdn.com/b/ericlippert/archive/2009/08/13/four-switch-oddities.aspx
+                            case "goto kludge":
+                                Console.WriteLine("Test cmd:" + Environment.NewLine + strCmd);
+                                break;
+                        }
+
+                        // Execute the command.
+                        if (strCmd.ToLower().StartsWith("use"))
+                        {
+                            string strDbPath = strCmd.Split()[1].TrimEnd(';');
+                            // Complete kludge.  Use regexp and check for xplat formats.
+                            if (strDbPath.StartsWith(@"C:\"))
+                                strParentDir = strDbPath;
                             else
+                                strParentDir = Utils.cstrHomeDir + System.IO.Path.DirectorySeparatorChar + strDbPath;
+                            dbTemp = new DatabaseContext(strParentDir);
+                            Console.WriteLine(strParentDir);
+                        }
+                        else if (strParentDir.Equals(""))
+                        {
+                            Console.WriteLine("Please select a database before executing a statement.");
+                        }
+                        else
+                        {
+							if (!string.IsNullOrWhiteSpace(strCmd))
+                            try
                             {
-                                try
-                                {
-                                    CommandParser parser = new CommandParser(dbTemp);
-                                    object objResult = parser.executeCommand(strCmd);
+                                CommandParser parser = new CommandParser(dbTemp);
+                                object objResult = parser.executeCommand(strCmd);
 
-                                    if (objResult is DataTable)
-                                    {
-                                        Console.WriteLine(InfrastructureUtils.dataTableToString((DataTable)objResult, intWindowWidth));
-                                    }
-                                    else if (objResult is string)
-                                    {
-                                        Console.WriteLine(objResult.ToString());
-                                    }
-                                    else if (strCmd.Trim().ToUpper().StartsWith("INSERT"))
-                                    {
-                                        Console.WriteLine("Row ID for new row is: " + objResult);
-                                    }
-                                    else if (strCmd.Trim().ToUpper().StartsWith("SELECT MAX("))
-                                    {
-                                        Console.WriteLine(objResult.ToString());
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Uncaptured return value.");
-                                    }
-                                }
-                                catch (Exception e)
+                                if (objResult is DataTable)
                                 {
-                                    Console.WriteLine("Error executing statement.\n\t" + e.Message);
+                                    Console.WriteLine(InfrastructureUtils.dataTableToString((DataTable)objResult, intWindowWidth));
                                 }
-
+                                else if (objResult is string)
+                                {
+                                    Console.WriteLine(objResult.ToString());
+                                }
+                                else if (strCmd.Trim().ToUpper().StartsWith("INSERT"))
+                                {
+                                    Console.WriteLine("Row ID for new row is: " + objResult);
+                                }
+                                else if (strCmd.Trim().ToUpper().StartsWith("SELECT MAX("))
+                                {
+                                    Console.WriteLine(objResult.ToString());
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Uncaptured return value.");
+                                }
                             }
-                        }   // end of "foreach" command in the split commands queue.
-                    }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Error executing statement.\n\t" + e.Message);
+                            }
+
+                        }
+                    }   // end of "foreach" command in the split commands queue.
                 }
                 catch (Exception ex)
                 {
